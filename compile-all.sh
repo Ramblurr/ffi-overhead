3a0a164e3eb728fc50b4619f9fc11873a3e05a4b
Casey Link <unnamedrambler@gmail.com>
2026-08-28 11:10:48 +0200

    build: move special-case toolchains into Tup

--- compile-all.sh
+++ compile-all.sh
@@ -1,29 +1,3 @@
 #!/bin/sh
 
-# Keep builds under project root
-export XDG_CACHE_HOME="$(pwd)/.cache"
-export NIMCACHE="$(pwd)/nimcache"
-
-# Set paths for Tupfile
-export DART_INCLUDE="$(dirname $(which dart))/../include"
-export NODE_INCLUDE="$(dirname $(which node))/../include/node"
-
-tup upd && \
-    jolt -Sdeps '{:paths ["jolt"]}' build \
-        -m ffi-overhead.jolt -o jolt_hello --opt --direct-link && \
-    rm -rf clojure_panama/classes && \
-    mkdir -p clojure_panama/classes && \
-    clojure -J--enable-native-access=ALL-UNNAMED \
-        -Sdeps '{:paths ["." "clojure_panama/classes"]}' \
-        -M -e '(binding [*compile-path* "clojure_panama/classes"] (compile (quote clojure-panama.hello)))' && \
-    rm -rf clojure_coffi/classes && \
-    mkdir -p clojure_coffi/classes && \
-    (cd clojure_coffi && \
-        clojure -J--enable-native-access=ALL-UNNAMED \
-            -M -e '(binding [*compile-path* "classes"] (compile (quote ffi-overhead.coffi)))') && \
-    nim c -d:release --parallelBuild:1 --nimcache:nimcache -o:nim_hello --passL:"-Lnewplus -lnewplus -Wl,-rpath,$$ORIGIN/newplus" hello.nim && \
-    zig build -Doptimize=ReleaseFast && \
-    tup upd && \
-    make -C ocaml && \
-    make -C elixir
-
+exec tup upd "$@"
